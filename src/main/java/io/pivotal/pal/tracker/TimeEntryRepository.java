7f0bccbb42f1d65f8f5eea5039689e6e241f62cb
Hannah Flynn <hmflynn781@gmail.com>
2019-10-02 07:24:23 -0600

    Refactoring project to use better OO design.

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -5,30 +5,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class TimeEntryRepository {
-    InMemoryTimeEntryRepository inMemoryRepository;
-
-    public TimeEntryRepository() {
-        this.inMemoryRepository = new InMemoryTimeEntryRepository();
-    }
-
-    public TimeEntry create(TimeEntry timeEntry) {
-        return this.inMemoryRepository.create(timeEntry);
-    }
-
-    public TimeEntry find(long timeEntryId) {
-        return this.inMemoryRepository.find(timeEntryId);
-    }
-
-    public List<TimeEntry> list() {
-        return this.inMemoryRepository.list();
-    }
-
-    public TimeEntry update(long timeEntryId, TimeEntry timeEntry) {
-        return this.inMemoryRepository.update(timeEntryId, timeEntry);
-    }
-
-    public void delete(long timeEntryId) {
-        this.inMemoryRepository.delete(timeEntryId);
-    }
+public interface TimeEntryRepository {
+    public TimeEntry create(TimeEntry timeEntry);
+    public TimeEntry find(long timeEntryId);
+    public List<TimeEntry> list();
+    public TimeEntry update(long timeEntryId, TimeEntry timeEntry);
+    public void delete(long timeEntryId);
 }
